e8f3004df6341c61b6e4306df83dfaa85979d8d3
Sneaker679 <benjamin.gauthier@hotmail.ca>
2024-05-31 19:18:11 -0400

    Working simulation of Schwarzschild

--- geodesics.py
+++ geodesics.py
@@ -2,18 +2,20 @@ from sympy import *
 
 # Geodesic covariant equation : ∂ₛuⱼ = 1/2 * ∂ⱼ(gₘₖ) * uᵐ uᵏ where uᵏ = ∂ₛxᵏ 
 def covariant_acc(s : Symbol, gₘₖ: Matrix, coordinates: list[Function]) -> Array:
-    dₛuⱼ: list(Function) = [0,0,0,0]
+    assert gₘₖ.shape[0] == len(coordinates)
+    dₛuⱼ: list[Function] = []
     uᵐ  : Array = Array(coordinates).diff(s)
     uᵐuᵏ: Array = tensorproduct(uᵐ,uᵐ)
 
     for j, coord in enumerate(coordinates):
-        dₛuⱼ[j] = 1/2 * tensorcontraction(tensorproduct(gₘₖ.diff(coord), uᵐuᵏ), (0,1,2,3))
+        dₛuⱼ.append(1/2 * tensorcontraction(tensorproduct(gₘₖ.diff(coord), uᵐuᵏ), (0,1,2,3)))
     
     dₛuⱼ= Array(dₛuⱼ)
     return simplify(dₛuⱼ)
 
 # Geodesic contravariant equation : ∂ₛuᵏ = ∂ₛ(gᵐᵏuₘ) = g where uᵏ = ∂ₛxᵏ
 def contravariant_acc(s : Symbol, gₘₖ: Matrix, coordinates: list[Function]) -> Array:
+    assert gₘₖ.shape[0] == len(coordinates)
     dₛuₘ_: Array  = covariant_acc(s, gₘₖ, coordinates)
     gᵐᵏ_ : Matrix = gₘₖ.inv()
     uʲ   : Array  = Array(coordinates).diff(s)
@@ -22,3 +24,14 @@ def contravariant_acc(s : Symbol, gₘₖ: Matrix, coordinates: list[Function]) 
     dₛuᵏ = tensorcontraction(tensorproduct( gᵐᵏ_       , dₛuₘ_ - gₘⱼuʲ ), (1,2))
     
     return simplify(dₛuᵏ)
+
+# Converts contravariant or covariant accelerations tensor arrays into lambda expressions, for later integration
+def to_lambda(s : Symbol, expressions : Array, coordinates : list[Function]):
+    assert expressions.shape[0] == len(coordinates)
+    dₛcoordinates : list[Function] = [coord.diff(s) for coord in coordinates]
+    coordinates.extend(dₛcoordinates)
+
+    lambda_functions : list[Function] = []
+    for k, expr in enumerate(expressions):
+        lambda_functions.append(lambdify(coordinates, expr, "scipy"))
+    return lambda_functions
--- run.py
+++ run.py
@@ -15,9 +15,10 @@ r = Function('r')(s)
 φ = Function('φ')(s)
 coordinates : list[Function] = [t, r, θ, φ]
 
+rs = 1
 gₘₖ = Matrix([
-    [1-1/r      ,0          ,0          ,0          ],
-    [0          ,(1/r-1)**-1,0          ,0          ],
+    [1-rs/r     ,0          ,0          ,0          ],
+    [0          ,1/(rs/r-1) ,0          ,0          ],
     [0          ,0          ,-r**2      ,0          ],
     [0          ,0          ,0          ,-r**2 * sin(θ)**2]
 ])
@@ -25,11 +26,59 @@ gₘₖ = Matrix([
 
 if __name__ == "__main__":
     dₛuⱼ= simplify(covariant_acc(s, gₘₖ, coordinates))
-    pprint(dₛuⱼ)
-
-    print()
-    print()
-    print()
-
     dₛuʲ= simplify(contravariant_acc(s, gₘₖ, coordinates))
     pprint(dₛuʲ)
+    equations : list = to_lambda(s, dₛuʲ,coordinates)
+
+    # Integration
+    from scipy.integrate import solve_ivp
+    from integrate import *
+    import numpy as np
+    ro = 4
+    initial = [0,ro,pi/2,0,3/(1-rs/ro),0,0,np.sqrt(3/(ro**2))-0.05]
+    values = solve_ivp(fun = diff_equations_system, t_span = (0,200), max_step = 0.1, y0 = initial, args=(equations,))
+    print(values)
+
+
+    # Checking is quantities are conserved
+    tol = 0.00001
+
+    # Energy (k)
+    dotT_init = values.y[4][0]
+    r_init = values.y[1][0]
+    k_init = dotT_init * (1 - rs/r_init)
+    for dotT, r in zip(values.y[4], values.y[1]):
+        k = dotT*(1-rs/r)
+        assert np.abs(k_init - k) < tol
+    print("Energy is conserved.")
+
+    # Angular momentum (h)
+    dotPhi_init = values.y[7][0]
+    h_init = r_init**2 * dotPhi_init
+    for dotPhi, r in zip(values.y[7], values.y[1]):
+        h = r**2 * dotPhi
+        assert np.abs(h_init - h) < tol
+    print("Angular momentum conserved.")
+
+
+    # Plotting
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as patches
+    x = []
+    y = []
+    for r,phi in zip(values.y[1],values.y[3]):
+        x.append(r*np.cos(phi))
+        y.append(r*np.sin(phi))
+    #plt.plot(values.t, values.y[1])
+    
+    fig, ax = plt.subplots()
+    ax.add_patch(patches.Circle((0,0), 1, edgecolor="black", fill=True))
+
+    plt.plot(x,y)
+    fig.set_size_inches(20, 20,forward=True)
+    plt.xlim(-16,16)
+    plt.ylim(-16,16)
+    fig.savefig("schwarzschild.pdf")
+    plt.show()
+
+
